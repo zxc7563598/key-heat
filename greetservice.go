12a52e249518b180460c2b6b824f2610feb65b36
zxc7563598 <junjie.he.925@gmail.com>
2026-04-13 10:30:34 +0800

    feat(frontend): 增加获取键盘布局方法，为前端提供键盘布局样式

--- greetservice.go
+++ greetservice.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"github.com/zxc7563598/key-heat/internal/storage"
+	"github.com/zxc7563598/key-heat/pkg/keymap"
 )
 
 type GreetService struct {
 	repo storage.Repository
 }
 
-func (g *GreetService) Greet(name string) string {
-	return "Hello " + name + "!"
+func (g *GreetService) GetKeyLayout(layoutType string) keymap.Layout {
+	return keymap.GetKeyLayout(keymap.LayoutANSI)
 }
