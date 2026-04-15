d17f93c165785edecd148f8689bf7cbf198207ef
zxc7563598 <junjie.he.925@gmail.com>
2026-04-15 14:57:56 +0800

    refactor(core): 完善热力图数据筛选

--- greetservice.go
+++ greetservice.go
@@ -16,7 +16,13 @@ func (g *GreetService) GetKeyLayout(layoutType keymap.LayoutType) keymap.Layout 
 }
 
 func (g *GreetService) GetHeatmap(start, end string) map[string]int {
-	data, err := g.repo.GetStatsByDateRange("2006-01-02", "2036-01-02")
+	if start == "" {
+		start = "2000-01-01"
+	}
+	if end == "" {
+		end = "2100-01-01"
+	}
+	data, err := g.repo.GetStatsByDateRange(start, end)
 	if err != nil {
 		log.Println("获取热力图数据失败")
 		return map[string]int{}
